f6d4871a46a4f8f78d076e041a3fbc13fa5b60a8
quandale-zingle-man <sjtestparts@gmail.com>
2026-02-09 09:10:49 +0000

    blog update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from "astro/config";
 import cloudflare from "@astrojs/cloudflare";
+import svelte from "@astrojs/svelte";
+import icon from "astro-icon";
 import { remarkEmoji } from "./src/utils/remark-emoji.ts";
 
 export default defineConfig({
   site: "https://t3rm1n4l.dev",
   adapter: cloudflare(),
   output: "server",
+  integrations: [icon(), svelte()],
   markdown: {
     remarkPlugins: [remarkEmoji],
   },
